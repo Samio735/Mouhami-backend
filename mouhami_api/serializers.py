590e6a2e7ad8b05cf1886967d94df9e525595ea5
lakhdari_abdallah <110565989+abdallahlakh@users.noreply.github.com>
2024-01-06 17:07:46 +0100

    Update serializers.py

--- mouhami_api/serializers.py
+++ mouhami_api/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
-from .models import Lawyer
+from .models import Lawyer,Language,Specialities
 
 class LawyerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lawyer
         fields = '__all__'
+
+
+
+class LanguageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Language
+        fields = '__all__'
+
+
+
+class SpecialitiesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Specialities
+        fields = '__all__'
+
