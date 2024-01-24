5308ae8ba0995ae22de26d0982bccdb01eb9703b
Said_Nouasria <saidnouasria23@gmail.com>
2024-01-24 14:31:18 +0100

    all bookings and reviews apis

--- mouhami_api/urls.py
+++ mouhami_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LawyerViewSet,booking,lawyerapi
+from .views import LawyerViewSet,booking,lawyerapi,mybookingslawyer,mybookingsuser
 
 urlpatterns = [
     path('lawyers/', LawyerViewSet.as_view()),
     path('lawyers/<int:id>', lawyerapi),
-    path('lawyers/<int:id>/bookings/',booking)
+    path('lawyers/<int:id>/bookings/',booking),
+    path('mybookingsuser/',mybookingsuser),
+    path('mybookingslawyer/',mybookingslawyer),
 ]
