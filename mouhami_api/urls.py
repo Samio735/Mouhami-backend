5499f74eb4b9c81adbf7af9c8534f43ea6e984da
Samy Rahim <102865808+Samio735@users.noreply.github.com>
2024-01-24 17:47:06 +0100

    Update urls.py

--- mouhami_api/urls.py
+++ mouhami_api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import LawyerViewSet,booking,lawyerapi
+from .views import LawyerViewSet,booking,lawyerapi,lawyerData
 
 urlpatterns = [
     path('lawyers/', LawyerViewSet.as_view()),
     path('lawyers/<int:id>', lawyerapi),
     path('lawyers/<int:id>/bookings/',booking),
-    path('import-lawyers/',views.lawyerData),
+    path('import-lawyers/',lawyerData),
 
 ]
